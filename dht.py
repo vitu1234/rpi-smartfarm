c957a506fe28f48b0b141e3c7f549c0e586e3da6
vitu1234 <mafeniblessed@gmail.com>
2022-12-29 22:02:18 +0900

    added mcp3008 sensor code

--- dht.py
+++ dht.py
@@ -6,24 +6,39 @@ import RPi.GPIO as GPIO
 import RPi.GPIO as gp  
 import datetime
 
+#mcp module and soil sensor
+
+import time
+import busio
+import digitalio
+import board
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
+
+
 #influxdb
 import influxdb_client
 from influxdb_client.client.write_api import SYNCHRONOUS
 
+#set soil sensor values
+spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+cs = digitalio.DigitalInOut(board.D5)
+mcp = MCP.MCP3008(spi, cs)
+channel = AnalogIn(mcp, MCP.P0) #soil sensor connected to channel 0
+
+#Calibraton values
+min_moisture = 26880
+max_moisture = 65472
 
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
 # Nf8h0NO5xQZMKx6nCcK0OtfLozuFngoREmynFKkIK_Zum3kcNw0F7AIjxWFAu5QwoO5Nz1N3RYgbxKD6zSOysA==
 
 # Initial the dht device, with data pin connected to:
 dhtDevice = adafruit_dht.DHT11(board.D4) 
 relay_ch = 17
-soil_sensor = 8
-
-# soil moisture
-# gp.setmode(gp.BOARD)  
-gp.setup(soil_sensor,gp.IN)   
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(relay_ch, GPIO.OUT)
 
 # you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
 # This may be necessary on a Linux single board computer like the Raspberry Pi,
@@ -38,6 +53,7 @@ gp.setup(soil_sensor,gp.IN)
 
 while True:
     try:
+        
         # Print the values to the serial port
         temperature_c = dhtDevice.temperature
         temperature_f = temperature_c * (9 / 5) + 32
@@ -64,32 +80,37 @@ while True:
         write_api = client.write_api(write_options=SYNCHRONOUS)
 
 
-        p = influxdb_client.Point("TemperatureSensor").tag("location", "sensor1").field("temperature", temperature_c)
-        write_api.write(bucket=bucket, org=org, record=p)
-        client.close()
+        if humidity is not None and temperature_c is not None:
+            p = influxdb_client.Point("TemperatureSensor").tag("location", "sensor1").field("temperature", temperature_c)
+            write_api.write(bucket=bucket, org=org, record=p)
+            client.close()
 
-        p = influxdb_client.Point("TemperatureSensor").tag("location", "sensor1").field("humidity", humidity)
-        write_api.write(bucket=bucket, org=org, record=p)
-        client.close()
+            p = influxdb_client.Point("TemperatureSensor").tag("location", "sensor1").field("humidity", humidity)
+            write_api.write(bucket=bucket, org=org, record=p)
+            client.close()
         
         
-        print('\nSoil Moisture\n')
+        print('Raw ADC Value: ', channel.value)
+        print('ADC Voltage: ' + str(channel.voltage) + 'V')
+        moisture = (max_moisture-channel.value)*100/(max_moisture-min_moisture) 
+        print("moisture: " + "%.2f" % moisture +"% (adc: "+str(channel.value)+")")
+        if moisture is not None:
+            p = influxdb_client.Point("SoilMoistureSensor").tag("data", "MoisturePercentage").field("moisture-percetage", moisture)
+            write_api.write(bucket=bucket, org=org, record=p)
+            client.close()
 
-        print(GPIO.input(soil_sensor))
-        print(not gp.input(soil_sensor))
-
-        if (not gp.input(soil_sensor))==0:
-            print('\n Soil is Wet - turn off pump\n')
+            if moisture >= 30:
+                print('\n Soil is Wet - turn off pump\n')
+                GPIO.output(relay_ch, GPIO.LOW)
+                # time.sleep(5)
+            else:
+                print('\n Soil is Dry - turn on pump\n')
+                # GPIO.setup(relay_ch, GPIO.OUT)
+                GPIO.output(relay_ch, GPIO.HIGH)
+            # GPIO.cleanup()   
+            GPIO.setwarnings(False)
+            GPIO.setmode(GPIO.BCM)
             GPIO.setup(relay_ch, GPIO.OUT)
-            GPIO.output(relay_ch, GPIO.HIGH)
-            # time.sleep(5)
-            
-        elif (not gp.input(soil_sensor))==1:
-            print('\n Soil is Dry - turn on pump\n')
-            GPIO.setup(relay_ch, GPIO.OUT)
-            GPIO.output(relay_ch, GPIO.LOW)
-            # time.sleep(5)
-
     except RuntimeError as error:
         # Errors happen fairly often, DHT's are hard to read, just keep going
         print(error.args[0])
@@ -99,7 +120,6 @@ while True:
         GPIO.cleanup()
         dhtDevice.exit()
         raise error
-    except KeyboardInterrupt:  
-        GPIO.cleanup()
+
     time.sleep(5.0)
 
